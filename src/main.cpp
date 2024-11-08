c38c9213380297d40e49e1c0f241863179417955
renehagen <renehagen@gmail.com>
2024-11-08 11:00:30 +0100

    Remove unnecessary header comments from main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,4 @@
 /**
- * @file display.ino
- * @author SeanKwok (shaoxiang@m5stack.com)
- * @brief M5StickCPlus2 Display Test
- * @version 0.1
- * @date 2023-12-09
- *
- *
  * @Hardwares: M5StickCPlus2
  * @Platform Version: Arduino M5Stack Board Manager v2.0.9
  * @Dependent Library:
